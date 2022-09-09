665d466edc360736aba41ea556b0156f543c3f58
Tiara Silva <tsilva7@uc.cl>
2022-09-09 12:21:26 -0300

    adding files

---
+++ constants.py
@@ -0,0 +1,59 @@
+#-------------------------- CONSTANTES ---------------------------#
+grey = 'aaabac'
+lightBlue = '8ba9d7'
+orange = 'c14811'
+blue = '22305a'
+white = 'ffffff'
+red = 'FF0000'
+
+filename = 'Plan irrestricto.xlsx'
+filename_asignacion = 'Colaboraciones plan de ventas/Asignación (venta - Producción).xlsx'
+filename_util = 'Colaboraciones plan de ventas/Fechas de zarpe - Logística.xlsx'
+filename_delay = 'Colaboraciones plan de ventas/DELAY.xlsx'
+filename_por_despachar = 'Colaboraciones plan de ventas/Pedidos por despachar.xlsx'
+filename_stock = 'Colaboraciones plan de ventas/Stock.xlsx'
+filename_por_producir = 'Colaboraciones plan de ventas/Por producir mes N.xlsx'
+filename_vol_cont = 'Colaboraciones plan de ventas/Volumen por contendor - Distribución Inter..xlsx'
+filename_puerto = 'Colaboraciones plan de ventas/Puerto.xlsx'
+
+PO = 'Pollo'
+PV = 'Pavo'
+GO = 'Cerdo'
+GA = 'Pollo'                                            # o Gallina
+
+oficina_plan_irrestricto = 'Agrosuper Shanghai'
+
+tamano = {
+  'Llave': 23,
+  'Sector': 8,                   
+  'Oficina': 15,             
+  'Material': 9,
+  'Descripción': 25,
+  'Nivel Jer. 2': 10,
+  'Nivel Jer. 3': 10,
+  'RV Producción mes n+1': 14,
+  'RV Venta mes n+1': 14,
+  '% Uti. producción': 7,
+  'Producción disponible': 14,
+  'Stock al día': 14,
+  'Por producir mes N': 14,
+  'Producción por despachar mes N': 14,
+  'Total disponible': 14,
+  'Delay': 14,
+  'Vol. prom. Por contenedor': 14,
+  'Atraso a facturar': 14,
+  'Ajuste atraso': 14,
+  'Facturación atraso': 14,
+  'Producción para venta nueva': 14,
+  'Venta del mes': 14,
+  'Ajuste venta nueva': 14,
+  'Facturación Venta nueva': 14,
+  'Saldo Volumen disponible': 14,
+  'Disponible stock sin venta': 14,
+  'Ajuste stock sin venta': 14,
+  'Facturación stock': 14,
+  'En puerto a facturar': 14,
+  'Plan Irrestricto (DATO)': 14,
+  'Plan Ajustado': 14,
+  'Motivo Ajuste': 25
+}
